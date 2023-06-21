b46526f2ea8bff7257436663efb05b4347c90e07
marie-ming <mingji408@gmail.com>
2023-06-21 17:30:55 +0900

    feat : 12강 이터러블 뜯어보기

--- 9.iterator/1.iterable.js
+++ 9.iterator/1.iterable.js
@@ -4,11 +4,20 @@
 // 순회 가능한 객체이다 라는 것을 알 수 있음
 // 순회가 가능하면 무엇을 할 수 있나? for..of, spread
 const array = [1, 2, 3];
-for (const item of array) {
+for (const item of array.entries()) {
   console.log(item);
 }
 
 const obj = { 0: 1, 1: 2 };
 for (const item in obj) {
   console.log(item);
+}
+
+const iterator = array.values();
+while (true) {
+  const item = iterator.next();
+
+  if (item.done) break;
+
+  console.log(item.value);
 }
